a83e48a46d0924cd500c7b9aa7ae5c3c86ba6816
Max <bulanovich5@seznam.cz>
2025-09-14 13:11:18 +0200

    seo, mobile viewport fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,36 +16,52 @@ import { Title, Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterModule,
-    HeaderComponent,
-    AboutUsComponent,
-    FooterComponent,
-    TechnologiesComponent,
-    OurServicesComponent,
-    WhyChooseUsComponent,
-    ContactsComponent,
-    WorkStepsComponent,
-    StepsComponent,
-    FaqComponent,
-  ],
+  imports: [RouterModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(private title: Title, private meta: Meta) {
+    // this.title.setTitle(
+    //   'UION — Tvoříme inovativní IT řešení na míru vašemu podnikání.'
+    // );
+    // this.meta.addTags([
+    //   {
+    //     name: 'description',
+    //     content: 'Tvoříme inovativní IT řešení na míru vašemu podnikání.',
+    //   },
+    //   {
+    //     name: 'keywords',
+    //     content: 'vývoj webových stránek, mobilní aplikace, Praha, IT',
+    //   },
+    // ]);
+  }
+  ngOnInit(): void {
     this.title.setTitle(
-      'UION — Tvoříme inovativní IT řešení na míru vašemu podnikání.'
+      'Inovativní IT řešení na míru | Vývoj webů, aplikací a design - UION'
     );
     this.meta.addTags([
       {
         name: 'description',
-        content: 'Tvoříme inovativní IT řešení na míru vašemu podnikání.',
+        content:
+          'Tvoříme inovativní IT řešení na míru vašemu podnikání. Neprodáváme kód - dodáváme kompletní řešení: webové stránky, mobilní aplikace a moderní design.',
       },
       {
         name: 'keywords',
-        content: 'vývoj webových stránek, mobilní aplikace, Praha, IT',
+        content:
+          'IT řešení, webové stránky, mobilní aplikace, UX design, vývoj na míru',
       },
+      {
+        property: 'og:title',
+        content: 'Inovativní IT řešení na míru | Vývoj webů, aplikací a design',
+      },
+      {
+        property: 'og:description',
+        content:
+          'Neprodáváme kód - dodáváme kompletní řešení: weby, mobilní aplikace a design.',
+      },
+      { property: 'og:image', content: 'https://uion.cz/assets/preview.jpg' },
+      { property: 'og:url', content: 'https://uion.cz' },
     ]);
   }
 }
